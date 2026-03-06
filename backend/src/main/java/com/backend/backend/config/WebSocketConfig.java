6c342649c02955006581e9b4492be80a6758bd4c
Kunal Dubey <xakep8@protonmail.com>
2026-03-06 08:50:24 +0530

    feat: Add additional allowed origins for WebSocket connections and configure Axios base URL using an environment variable.

--- backend/src/main/java/com/backend/backend/config/WebSocketConfig.java
+++ backend/src/main/java/com/backend/backend/config/WebSocketConfig.java
@@ -39,7 +39,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:5173")
+                .setAllowedOrigins("http://localhost:5173", "https://chat.obiwancodes.in", "http://127.0.0.1:5173")
                 .withSockJS();
     }
 
--- chat_app/src/api/axios.ts
+++ chat_app/src/api/axios.ts
@@ -3,7 +3,7 @@ import { useAuthStore } from '../store/useAuthStore';
 
 // Create an Axios instance
 const api = axios.create({
-    baseURL: '', // The Vite proxy will handle forwarding to VITE_API_ENDPOINT
+    baseURL: import.meta.env.VITE_API_ENDPOINT, // The Vite proxy will handle forwarding to VITE_API_ENDPOINT
     headers: {
         'Content-Type': 'application/json',
     },
